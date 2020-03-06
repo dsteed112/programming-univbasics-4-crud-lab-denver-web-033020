14dd4d0ae8f44e2d685b5dc208c34b1637877b8e
Daniel Steed <dsteed112@gmail.com>
2020-03-06 23:02:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,36 @@
+
+
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  starwars = ["luke", "han", "lea", "chewy"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["yo", "man"]
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["yo", "man"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["yo", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow", "man"]
+  array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(array, 2)
+  array = ["wow", "man", "dude"]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
